d955b2d944d1ee8f34470ad25b56096a9e699683
suleymaninci <suleymann25@gmail.com>
2019-03-22 00:49:42 +0300

    Add files via upload

---
+++ BinarySymmetricChannel.m
@@ -0,0 +1,5 @@
+function [y] = BinarySymmetricChannel(x,epsilon);
+    %x=ChannelEncoder(b,L);
+    y=bsc(x,epsilon);%%bsc func is a one of functions in Comm toolbox
+    
+    end
---
+++ MajorityDecoder.m
@@ -0,0 +1,5 @@
+function d = MajorityDecoder(y,L)
+
+d = mode(reshape(y,L,[]))
+
+end
