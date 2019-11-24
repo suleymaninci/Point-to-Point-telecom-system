299b6c060fb1dd73f239db6ca23f923d92d539e8
suleymaninci <suleymann25@gmail.com>
2019-11-25 01:39:55 +0300

    Delete proberr.m

--- proberr.m
+++
@@ -1,8 +0,0 @@
-function [p_e]=proberr(L,epsilon)
-syms k
-p_e = symsum(((factorial(L)/(factorial(round(k))*factorial(L-round(k))))*(epsilon^k)*((1-epsilon)^(L-k))), k, round(L/2), L);
-%3rd and 4th variable of symsum is sum of series bounds such as for L=3...
-%round(L/2)=2 so our k variable change in 2 and 3.
-%%symsum is sum of series function in MATLAB. We just taken sum of series
-%%%function in matlab
-end
